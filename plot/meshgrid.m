5a57168e4dc020d69fc71cdc53234b0341020212
cadlab3 <cadlab3@DESKTOP-NN3USFB>
2022-08-25 22:40:13 +0900

    meshgrid 이용하여 3d 그래프 그리기

--- plot/meshgrid.m
+++ plot/meshgrid.m
@@ -1,4 +1,15 @@
-x = 1:10;
-y = 1:10;
+x = linspace(0,2*pi, 100);
+y = linspace(0,2*pi, 100);
+
 
 [X,Y] = meshgrid(x,y)
+
+Z = cos(X) + sin(Y);
+
+mesh(X,Y,Z)
+
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+colorbar;
